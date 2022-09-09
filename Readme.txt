5d8f09b5381899553a789a0769e6a017e35e8ebc
Poolce <poolce@list.ru>
2022-09-09 14:58:12 +0300

    master

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+1) Запустить сервер (npm start)
+2) Открыть файл home.html
+3) Отправить POST запрос в виде https://yandex.ru/,http://habrahabr.ru
--- src/routes/home.js
+++ src/routes/home.js
@@ -5,29 +5,24 @@ const Hapi = require('@hapi/hapi');
 
 
 async function response(req, h) {
-  let url = 
-  [
-    'https://stackoverflow.com/questions/9168737/read-a-text-file-using-node-js',
-    'https://learn.javascript.ru/promise?ysclid=l7hrjy152a262632226',
-    'https://habr.com/ru/post/435084/?ysclid=l7hshp9oz1328557447',
-    'https://infostart.ru/1c/articles/812886/?ysclid=l7hnxl4a6l651953880'
-  ];
 
-
+  console.log(req.payload);
+  let url = [];
+  url = await req.payload.text.split(',');
+  
   let result = await funs.getResult(url);
 
 
   return h.response(result)
     .header('Content-Type', 'application/pdf')
-    .header('Content-Disposition', 'attachment; filename= ' + 'file.pdf');
+    .header('Content-Disposition', 'attachment; filename= ' + 'response.pdf');
 
 }
   
   module.exports = {
-    method: 'GET', // Метод
-    path: '/', // Путь
+    method: 'POST', // Метод
+    path: '/res', // Путь
     options: { 
       handler: response,
     }
-    
   };
